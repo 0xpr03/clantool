f72d646c99bf62a6f1d1ddcda09d2765c99edca1
Aron Heinecke <Ox0p54r36@t-online.de>
2025-08-16 20:20:28 +0200

    ci fixes

--- test.sh
+++ test.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
+set -x
 
+printenv -0
 cargo build --verbose
 
-mysql -e 'DROP DATABASE IF EXISTS test;'
-mysql -e 'CREATE DATABASE test;'
+mysql -h $MYSQL_HOST -u $USER -p$MYSQL_PWD -e 'DROP DATABASE IF EXISTS test;'
+mysql -h $MYSQL_HOST -u $USER -p$MYSQL_PWD -e 'CREATE DATABASE test;'
 RUST_BACKTRACE=1 cargo test -- --test-threads=1
-mysql -e 'DROP DATABASE IF EXISTS test;'
-mysql -e 'CREATE DATABASE test;'
+mysql -h $MYSQL_HOST -u $USER -p$MYSQL_PWD -e 'DROP DATABASE IF EXISTS test;'
+mysql -h $MYSQL_HOST -u $USER -p$MYSQL_PWD -e 'CREATE DATABASE test;'
 
 # init config
 cargo run --bin clantool -- --help || true
 sed -i -e 's/"user"/"root"/g' config/config.toml
-sed -i -e 's/password = "password"//g' config/config.toml
+sed -i -e 's/password = "password"/password = "root"/g' config/config.toml
 sed -i -e 's/"clantool"/"test"/g' config/config.toml
 cat config/config.toml
 
@@ -20,4 +22,4 @@ cat config/config.toml
 RUST_BACKTRACE=1 cargo run --bin clantool -- init
 # fails in CI
 #RUST_BACKTRACE=1 cargo run --bin clantool -- fcrawl || true
-mysql -e 'DROP DATABASE test;'
+mysql -h $MYSQL_HOST -u $USER -p$MYSQL_PWD -e 'DROP DATABASE test;'
